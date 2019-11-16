e38c46a90cb2a9a7ef97681350a685b38b7788e4
Dmitriy <da.savushkin4@gmail.com>
2019-11-16 05:30:25 +0300

    Initial Commit

---
+++ MobileApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileApp
+//
+//  Created by Дмитрий on 16/11/2019.
+//  Copyright © 2019 Дмитрий. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
